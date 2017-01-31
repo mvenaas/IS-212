8f5097fd5df37a1e9402706992252f57fa7d35c1
Erlend W <zw@zwirc.com>
2017-01-31 15:19:11 +0100

    Blabla

--- mandatory_01/AlgorithmAnalysis1/src/is211/alganalysis/InsertSortAnalysis.java
+++ mandatory_01/AlgorithmAnalysis1/src/is211/alganalysis/InsertSortAnalysis.java
@@ -15,25 +15,21 @@ public class InsertSortAnalysis extends EmpiricalAnalysis {
         super("Insertion sort");
     }
 
-
-
     protected void run(int[] shuffledData) {
         insertSort(shuffledData);   
     }
-
+    
     public void insertSort(int[] data) {
         int n = data.length;
-
         //outer loop picks the next value to insert
         for (int i = 1; i < n; i++) {
             //inner loop inserts it
             int nextval = data[i];
             int j = i-1;
+            super.assign(nextval);
 //            while (j >= 0 && data[j] > nextval) {
              while (j >= 0 && super.greaterThan(data[j], nextval)) {
                 data[j + 1] = data[j];
-// Add value to super Assign.
-                super.assign(j);
                 j--;              
             }
             data[j+1] = nextval;
